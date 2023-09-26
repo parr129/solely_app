b24bcd6202be2e47596dc3257fe90f26f06251a0
Jaiditya Batra <jaiditya7318@gmail.com>
2023-09-26 23:53:04 +0530

    Add AI detection in review

--- app.js
+++ app.js
@@ -1,50 +1,43 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+import { HfInference } from "../../node_modules/@huggingface/inference/dist/index.mjs";
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const HF_ACCESS_TOKEN = "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";
 
-// Define your routes and NLP functionality here
+const hf = new HfInference(HF_ACCESS_TOKEN);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const analyzeButton = document.getElementById("analyzeButton");
+    const reviewText = document.getElementById("reviewText");
+    const resultDiv = document.getElementById("result");
 
-const { pipeline } = require('@huggingface/node');
-const apiKey = 'YOUR_HUGGING_FACE_API_KEY'; // Replace with your Hugging Face API key
+    analyzeButton.addEventListener("click", async () => {
+        const review = reviewText.value.trim();
 
-const nlp = pipeline('sentiment-analysis', { model: 'openai/roberta-base-sentiment', apiKey });
+        if (review === "") {
+            alert("Please enter a review before analyzing.");
+            return;
+        }
 
+        try {
+            // Perform the analysis using Hugging Face model
+            const result = await hf.textClassification({
+                model: "roberta-base-openai-detector",
+                inputs: review,
+            });
 
-async function detectBotReview(reviewText) {
-    try {
-        const results = await nlp(reviewText);
-        const sentimentScore = results[0].score;
+            let resultHTML = '<p>Review Analysis Result:</p>';
 
-        // You can set a threshold for bot review detection based on sentiment score
-        const isBotReview = sentimentScore < 0.3;
+            result.forEach((item, index) => {
+                resultHTML += `
+            <p>Result ${index + 1}:</p>
+            <p>Label: ${item.label}</p>
+            <p>Score: ${item.score}</p>
+        `;
+            });
 
-        return isBotReview;
-    } catch (error) {
-        console.error('Error analyzing review:', error);
-        return false;
-    }
-}
-
-
-app.post('/analyze-review', async (req, res) => {
-    const { reviewText } = req.body;
-
-    if (!reviewText) {
-        return res.status(400).json({ error: 'Review text is required' });
-    }
-
-    const isBotReview = await detectBotReview(reviewText);
-
-    if (isBotReview) {
-        return res.json({ result: 'Bot review detected' });
-    } else {
-        return res.json({ result: 'Review looks good' });
-    }
+            resultDiv.innerHTML = resultHTML;
+        } catch (error) {
+            console.error("Error analyzing the review:", error);
+            resultDiv.innerHTML = "An error occurred while analyzing the review.";
+        }
+    });
 });
